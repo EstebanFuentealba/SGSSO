441eef89b21a3d57b35c285b1810c78945efb63a
Esteban Fuentealba <mi.warezx@gmail.com>
2011-11-09 01:15:29 -0300

    . datastore grafico

---
+++ SRC/DOT NET/WCF_ENAP/WS/GraphEventosOrganizacion.cs
@@ -0,0 +1,60 @@
+﻿/**
+*Imports.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using WCF_ENAP.WS;
+using System.Web;
+
+namespace WCF_ENAP
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class GraphEventosOrganizacion
+    {
+        private DataClassesEnapDataContext bd;
+
+        public GraphEventosOrganizacion()
+        {
+            bd = new DataClassesEnapDataContext();
+        }
+        [WebGet(UriTemplate = "?page={_page}&start={_start}&limit={_limit}&sort={_sort}&dir={_dir}&ID_ORGANIZACION={ID_ORGANIZACION}&ANO={ANO}")]
+        public JSONCollection<List<sp_get_eventos_departamento_by_organizacionResult>> GetCollection(int _page, 
+                int _start, 
+                int _limit, 
+                string _sort, 
+                string _dir,
+                int ID_ORGANIZACION,
+                int ANO)
+        {
+            JSONCollection<List<sp_get_eventos_departamento_by_organizacionResult>> objJSON = new JSONCollection<List<sp_get_eventos_departamento_by_organizacionResult>>();
+            if (ID_ORGANIZACION == 0)
+            {
+                ID_ORGANIZACION = 1;
+            }
+            if (ANO == 0)
+            {
+                ANO = DateTime.Now.Year;
+            } 
+            if (_page == 0)
+                {
+                    _page = 1;
+                }
+                if (_limit == 0)
+                {
+                    _limit = 10;
+                }
+                _start = (_page * _limit) - _limit;
+                var query = bd.sp_get_eventos_departamento_by_organizacion(ID_ORGANIZACION, ANO, _start, _limit);
+            objJSON.items = query.ToList<sp_get_eventos_departamento_by_organizacionResult>();
+            objJSON.success = true;
+            return objJSON;
+        }
+    }
+}
---
+++ SRC/DOT NET/WCF_ENAP/app/store/dsGraphEventosOrganizacion.js
@@ -0,0 +1,34 @@
+﻿Ext.define('WCF_ENAP.model.GraphEventosOrganizacion', {
+    extend: 'Ext.data.Model',
+    fields: [
+        { "name": "MES_NAME", "type": "string" },
+        { "name": "MES_ID", "type": "int" },
+        { "name": "COUNT_EVENTOS", "type": "int" }
+    ]
+});
+Ext.define('WCF_ENAP.store.dsGraphEventosOrganizacion', {
+    extend: 'Ext.data.Store',
+
+    constructor: function (cfg) {
+        var me = this;
+        cfg = cfg || {};
+        me.callParent([Ext.apply({
+            autoLoad: false,
+            autoSync: false,
+            storeId: 'dsGraphEventosOrganizacion',
+            pageSize: 20,
+            remoteSort: true,
+            model: 'WCF_ENAP.model.GraphEventosOrganizacion',
+            proxy: {
+                type: 'rest',
+                url: '/GraphEventosOrganizacion/',
+                reader: {
+                    type: 'json',
+                    root: 'items',
+                    totalProperty: 'totalCount',
+                    successProperty: 'success'
+                }
+            }
+        }, cfg)]);
+    }
+});
